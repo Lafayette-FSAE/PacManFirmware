6c428f06fe22e294ab654b2cf71b1d8d7ed57408
CwbhX <cwbh10@gmail.com>
2019-10-30 15:46:46 -0400

    Added skeleton files and readme

---
+++ Core1.cpp
@@ -0,0 +1,30 @@
+/*
+  Core1.cpp - Library for PacMan Core 1.
+  Created by Clement Hathaway & Simone Khalifa, October 30, 2019.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "Core1.h"
+
+// Core1::Core1(int pin)
+// {
+//   pinMode(pin, OUTPUT);
+//   _pin = pin;
+// }
+//
+// void Core1::dot()
+// {
+//   digitalWrite(_pin, HIGH);
+//   delay(250);
+//   digitalWrite(_pin, LOW);
+//   delay(250);
+// }
+//
+// void Core1::dash()
+// {
+//   digitalWrite(_pin, HIGH);
+//   delay(1000);
+//   digitalWrite(_pin, LOW);
+//   delay(250);
+// }
---
+++ PacMan.ino
@@ -0,0 +1,12 @@
+#include <Core0.h>
+#include <Core1.h>
+
+void setup() {
+// put your setup code here, to run once:
+
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+    printf()
+}
