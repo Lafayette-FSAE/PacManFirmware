0a5612cae2993fc6e0568a31d5f9f5733f17bcfe
simaloo <simakhalifa@gmail.com>
2019-12-13 19:31:59 -0500

    Update PacMan.ino

--- PacMan/PacMan.ino
+++ PacMan/PacMan.ino
@@ -1,26 +1,52 @@
-//#include "Core0.h"
+<<<<<<< Updated upstream
+//#include <Core0.h>
+#include "Core1.h"
+
+
+void setup() {
+// put your setup code here, to run once:
+
+=======
+#include "Core0.h"
 #include "Core1.h"
 
 // GLOBALS
-struct cell cells[16];             // Written to by Core1, read by Core0
-struct cellConfig cellConfigs[16]; // Written to by Core0, read by Core1 for CAN Bus
-float miscConfigs[6];              // Written to by Core0, read by Core1 for CAN Bus
-boolean externalFault;             // Written to by Core1 from CAN Bus, read by Core0
-boolean AIRSOpen;                  // Written to by Core0, read by Core1 for CAN Bus
+struct cell cells[16];               // Written to by Core1, read by Core0
+float externalFault;                 // Written to by Core1 from CAN Bus, read by Core0
+boolean AIRSOpen;                    // Written to by Core0, read by Core1 for CAN Bus
+//struct cellConfig cellConfigs[16]; // Written to by Core0, read by Core1 for CAN Bus    --to be added as a later feature
+//float miscConfigs[6];              // Written to by Core0, read by Core1 for CAN Bus    --to be added as a later feature
 
 // SEMAPHORES
-//SemaphoreHandle_t cellArraySem;
-//SemaphoreHandle_t cellConfigSem;
-//SemaphoreHandle_t miscConfigSem;
-//SemaphoreHandle_t externalFaultSem;
-//SemaphoreHandle_t AIRSOpenSem;
+TaskHandle_t Task1, Task2;
+SemaphoreHandle_t cellArraySem;
+SemaphoreHandle_t externalFaultSem;
+SemaphoreHandle_t AIRSOpenSem;
 
 void setup() {
-  // put your setup code here, to run once:
-  //Core0.run(); //add task stuff
-  //Core1.run(); //add task stuff
+  cellArraySem = xSemaphoreCreateMutex();
+  externalFaultSem = xSemaphoreCreateMutex();
+  AIRSOpenSem = xSemaphoreCreateMutex();
+
+  xTaskCreatePinnedToCore(&codeForTask1, "Core0Task", 1000, NULL, 1, &Task1, 0);
+  delay(500);
+  xTaskCreatePinnedToCore(&codeForTask2, "Core1Task", 1000, NULL, 1, &Task2, 1);
+>>>>>>> Stashed changes
 }
 
 void loop() {
-  delay(100); //delay is so that loop does not eat up CPU time
+// put your main code here, to run repeatedly:
+    printf()
+}
+
+void codeForTask1( void * parameter )
+{
+    Core0 core0 (cells, externalFault, AIRSOpen, cellArraySem, externalFaultSem, AIRSOpenSem);
+    core0.start();
+}
+
+void codeForTask2( void * parameter )
+{
+    Core1 core1 (cells, externalFault, AIRSOpen, cellArraySem, externalFaultSem, AIRSOpenSem);
+    core1.start();
 }
